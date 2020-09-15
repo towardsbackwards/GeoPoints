449fe92dbd357a9d44131d73ffcc4fca744166ba
towardsbackwards <Protuberance23>
2020-09-16 00:32:01 +0300

    A star checkpoint #9 View GeoJSON-look-a-like return

--- mainapp/views.py
+++ mainapp/views.py
@@ -10,6 +10,18 @@ from django.core.serializers import serialize
 
 from mainapp.algorithm import best_path_by
 
+answer_blank = {"type": "FeatureCollection",
+          "features": [
+              {"type": "Feature",
+               "geometry": {
+                   "type": "LineString",
+               },
+               "properties": {
+               }
+               },
+          ]
+          }
+
 
 class MinLength(APIView):
 
@@ -17,18 +29,26 @@ class MinLength(APIView):
         point_from = self.kwargs['from']
         point_to = self.kwargs['to']
         result_km = best_path_by(point_from, point_to, Line, Point)
-        result_score = best_path_by(point_from, point_to, Line, Point, eval_type='by_score')
         print(result_km)
-        print(result_score)
-        geojson_answer = json.loads(
-            serialize('geojson', Point.objects.filter(id__in=result_km['path']),
-                      geometry_field='geom',
-                      fields=['pk', 'score', 'geom']))
-        return Response({"points": geojson_answer})
+        geojson_coords = answer_blank['features'][0]['geometry']['coordinates'] = []
+        for i in Point.objects.filter(id__in=result_km['path']):
+            geojson_coords.append([i.geom[0], i.geom[1]])
+        answer_blank['features'][0]['properties']['name'] = 'Shortest path'
+        return Response({"answer": answer_blank})
 
 
 class MinScore(APIView):
-    pass
+
+    def get(self, request, **kwargs):
+        point_from = self.kwargs['from']
+        point_to = self.kwargs['to']
+        result_score = best_path_by(point_from, point_to, Line, Point, eval_type='by_score')
+        print(result_score)
+        geojson_coords = answer_blank['features'][0]['geometry']['score_points'] = []
+        for i in Point.objects.filter(id__in=result_score['path']):
+            geojson_coords.append([i.score])
+        answer_blank['features'][0]['properties']['name'] = 'Cheapest path'
+        return Response({"answer": answer_blank})
 
 
 class PointsView(APIView):
