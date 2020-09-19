6fec52cf349e14697bf7a5fb73f47a3d660bcc05
towardsbackwards <Protuberance23>
2020-09-19 14:11:14 +0300

    GeoJSON length points order fix output

--- mainapp/algorithm.py
+++ mainapp/algorithm.py
@@ -4,8 +4,11 @@ import collections
 
 # инициализация именований индексов для значений узлов
 # Представление каждого узла как list, упорядочивая их в куче по по формуле
-# f = стартовая стоимость (g) + значение эвристики для текущего узла (h)
+# F = стартовая стоимость (G) + значение эвристики для текущего узла (H)
 # Чем меньше это значение - тем приоритетней точка (узел)
+# Узлы в heap упорядочены по значению F. В случае конфликта
+# одинаковых значений F упорядочивание будет определено по NUM
+# т.к. в куче кортежи сравниваются поэлементно слева направо.
 F, H, NUM, G, POS, OPEN, VALID, PARENT = range(8)
 
 
@@ -17,7 +20,7 @@ def best_path_by(start_point, end_point, line_model, point_model, eval_type='by_
             start_point     - id стартовой точки (int)
             end_point       - id конечной точки (int)
             point_model     - Django-модель точки
-            point_model     - Django-модель линии (с FK к point model - from_point & to_point)
+            line_model     - Django-модель линии (с FK к point model - from_point & to_point)
             eval            - Способ вычисления (by_distance - поиск пути по кратчайшему расстоянию;
                                                 by_score - по минимальному количеству баллов)"""
     depend_nodes = collections.defaultdict(list)
@@ -77,51 +80,54 @@ def best_path_by(start_point, end_point, line_model, point_model, eval_type='by_
         start_h = heuristic(start_pos)
         start_node = [start_cost + start_h, start_h, next(nums), start_cost, start_pos, True,
                       True, None]
-        closed_nodes = {start_pos: start_node}
-        open_heap = [start_node]
+        watched_nodes = {start_pos: start_node}
+        nodes_heap = [start_node]
         best = start_node
-        while open_heap:
-            current = heappop(open_heap)
-            current[OPEN] = False
+        while nodes_heap:
+            current = heappop(nodes_heap)  # получаем узел с минимальной стоимостью
+                                          # (вначале это будет start_node), одновременно убирая его из nodes_heap
+            current[OPEN] = False         # делаем узел закрытым
             if current[POS] == goal_point:
                 best = current
                 break
-            for neighbor_pos in neighbors(current[POS]):
-                new_neighbor_g = current[G] + cost(current[POS], neighbor_pos)
-                neighbor = closed_nodes.get(neighbor_pos)
-                if neighbor is None:
-                    if len(closed_nodes) >= limit:
-                        continue
-                    neighbor_h = heuristic(neighbor_pos)
+            for i in neighbors(current[POS]):
+                new_neighbor_g = current[G] + cost(current[POS], i)  # полная стоимость этого соседа
+                neighbor = watched_nodes.get(i)
+                if neighbor is None:  # если мы смотрим этого соседа впервые (проверка для экономии ресурсов)
+                    if len(watched_nodes) >= limit:
+                        continue  # на случай достижения лимита 9223372036854775807 в куче продолжаем цикл
+                    neighbor_h = heuristic(i)   # считаем эвристику для этого соседа
                     neighbor = [new_neighbor_g + neighbor_h, neighbor_h, next(nums),
-                                new_neighbor_g, neighbor_pos, True, True, current[POS]]
-                    closed_nodes[neighbor_pos] = neighbor
-                    heappush(open_heap, neighbor)
+                                new_neighbor_g, i, True, True, current[POS]]
+                    watched_nodes[i] = neighbor    # добавляем посчитанный узел в просмотренные
+                    heappush(nodes_heap, neighbor)  # как открытый (i = номер узла в БД)
                     if neighbor_h < best[H]:
-                        best = neighbor  # Эвристика нового узла меньше. Мы приближаемся к цели.
-                elif new_neighbor_g < neighbor[G]:
-                    if neighbor[OPEN]:
-                        neighbor[VALID] = False
-                        closed_nodes[neighbor_pos] = neighbor = neighbor[:]
+                        best = neighbor  # Эвристика нового узла меньше. Обозначили его ближайшим к цели на текущий момент.
+                elif new_neighbor_g < neighbor[G]:  # если стоимость до ранее просмотренного узла была больше, чем сейчас
+                    if neighbor[OPEN]:  # соседний узел открыт - его соседи не раскрыты
+                        neighbor[VALID] = False  # бракуем ранее просмотренный узел - FALSE
+                        watched_nodes[i] = neighbor = neighbor[:]  # взяли копию узла из кучи как neighbor
+                        #  обновляем значения узла на текущие и кладем в очередь
                         neighbor[F] = new_neighbor_g + neighbor[H]
                         neighbor[NUM] = next(nums)
                         neighbor[G] = new_neighbor_g
                         neighbor[VALID] = True
                         neighbor[PARENT] = current[POS]
-                        heappush(open_heap, neighbor)
-                    else:
+                        heappush(nodes_heap, neighbor)
+                    else:  # соседний узел закрыт - меняем его родителя, F и G и кладем в очередь
                         neighbor[F] = new_neighbor_g + neighbor[H]
                         neighbor[G] = new_neighbor_g
                         neighbor[PARENT] = current[POS]
                         neighbor[OPEN] = True
-                        heappush(open_heap, neighbor)
-            while open_heap and not open_heap[0][VALID]:
-                heappop(open_heap)
+                        heappush(nodes_heap, neighbor)
+            while nodes_heap and not nodes_heap[0][VALID]:  # удаляем невалидные лидирующие узлы
+                heappop(nodes_heap)
         path = []
+        # восстанавливаем путь от конечного узла до начального (current - конечный узел)
         current = best
         while current[PARENT] is not None:
             path.append(current[POS])
-            current = closed_nodes[current[PARENT]]
+            current = watched_nodes[current[PARENT]]
         path.append(start_pos)
         path.reverse()
         return path
